6dd202627511952a41751db291535f557d45b109
mooss <mooss@protonmail.com>
2025-05-25 20:54:37 +0200

    Replace darkokai with monokai
    Org-mode code blocks were rendered as comments.

--- init.el
+++ init.el
@@ -10,11 +10,18 @@
  '(custom-safe-themes
    '("aa545934ce1b6fd16b4db2cf6c2ccf126249a66712786dd70f880806a187ac0b"
      default))
- '(package-selected-packages nil)
+ '(package-selected-packages
+   '(all-the-icons darkokai-theme diminish doom-modeline magit
+		   monokai-theme ob-go org-bullets treemacs undo-tree
+		   vertico yasnippet))
  '(package-vc-selected-packages '((ob-go :url "https://github.com/mooss/ob-go"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(org-level-1 ((t (:inherit outline-1 :height 1.0))))
+ '(org-level-2 ((t (:inherit outline-2 :height 1.0))))
+ '(org-level-3 ((t (:inherit outline-3 :height 1.0))))
+ '(org-level-4 ((t (:inherit outline-4 :height 1.0))))
+ '(org-level-5 ((t (:inherit outline-5 :height 1.0)))))
