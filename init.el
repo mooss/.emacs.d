5cb70f333ba69e97e3dbe9e5903aff1392a46f7b
mooss <mooss@protonmail.com>
2025-06-18 19:43:55 +0200

    Disable the corfu popup when moving the cursor horizontally

--- init.el
+++ init.el
@@ -9,15 +9,14 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("aa545934ce1b6fd16b4db2cf6c2ccf126249a66712786dd70f880806a187ac0b"
-	 default))
+     default))
  '(package-selected-packages
-   '(all-the-icons color-identifiers-mode consult corfu darkokai-theme
-				   diminish doom-modeline eldoc-box go-mod-mode
-				   go-mode gptel helpful js-mode magit marginalia
-				   markdown-mode monokai-theme ob-go orderless
-				   org-bullets persistent-scratch rg sudo-edit
-				   tree-sitter treemacs treesit-auto undo-tree vertico
-				   yasnippet))
+   '(all-the-icons color-identifiers-mode consult corfu diminish
+                   doom-modeline go-mode gptel helpful magit
+                   marginalia markdown-mode monokai-theme ob-go
+                   orderless org-bullets persistent-scratch rg
+                   sudo-edit treemacs undo-tree vertico yaml yaml-mode
+                   yasnippet))
  '(package-vc-selected-packages '((ob-go :url "https://github.com/mooss/ob-go"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
